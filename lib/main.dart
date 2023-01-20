3300b6fefce3bc82a6dd0d75851da1b58c37a639
Your Name <you@example.com>
2023-01-20 14:22:54 +0545

    final commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:captcha/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
+
+import 'provider/google_sign_in.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+final navigatorKey = GlobalKey<NavigatorState>();
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  // ignore: must_call_super
+  void initState() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child: GetMaterialApp(
+        title: 'Shoe Wonder',
+        debugShowCheckedModeBanner: false,
+        navigatorKey: navigatorKey,
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.white,
+        ),
+        home: const Splash(),
+      ),
+    );
+  }
+}
